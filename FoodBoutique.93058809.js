eff916a779e4843632a9918c002065aed40691af
Pizzaman333 <andrijmalchevv369@gmail.com>
2025-05-18 01:46:28 +0300

    Updates

---
+++ FoodBoutique.93058809.js
@@ -0,0 +1,2 @@
+document.querySelector(".hero__circle").addEventListener("click",e=>document.querySelector(".hero__circle--text").classList.toggle("hero__circle--rotating"));
+//# sourceMappingURL=FoodBoutique.93058809.js.map
