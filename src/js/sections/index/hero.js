78f2fd89ba4ba08c6b5f0a24f5ef19a5f1cbcb4a
Pizzaman333 <andrijmalchevv369@gmail.com>
2025-05-18 12:53:30 +0300

    scroll to top added

--- src/js/sections/index/hero.js
+++ src/js/sections/index/hero.js
@@ -1,1 +1,1 @@
-document.querySelector(".hero__circle").addEventListener("click", (e) => document.querySelector(".hero__circle--text").classList.toggle("hero__circle--rotating"));
+// document.querySelector(".hero__circle").addEventListener("click", (e) => document.querySelector(".hero__circle--text").classList.toggle("hero__circle--rotating"));
