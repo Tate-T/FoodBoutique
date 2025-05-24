35c004f661de853a8884a6813439b5c5c550e641
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2025-05-25 00:50:04 +0300

    category

--- src/js/sections/index/products.js
+++ src/js/sections/index/products.js
@@ -1,7 +1,7 @@
 const getFilteredProducts = async (keyword, category, id , sort) => {
   try {
     return await fetch(
-      `https://food-boutique.b.goit.study/api/products?keyword=${keyword}${sort}&category=${category}&page=${id}&limit=9`
+      `https://682dfb9c746f8ca4a47b717c.mockapi.io/foodboutique/products${keyword}${sort}&category=${category}&page=${id}&limit=9`
     ).then((response) => response.json());
   } catch (e) {
     return e;
