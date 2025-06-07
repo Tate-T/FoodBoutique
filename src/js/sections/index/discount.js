77426ad15064a2ca5e417fab55bc743abd0489f5
DaniilKhlivniuk1 <141495344+DaniilKhlivniuk1@users.noreply.github.com>
2025-06-08 01:01:44 +0300

    n

---
+++ src/js/sections/index/discount.js
@@ -0,0 +1,40 @@
+import { getDiscountProducts } from "../../fetchs/getDiscountProducts";
+getDiscountProducts().then((products) => {
+    let markUp = "";
+    for (let index = 0; index < 2; index+= 1) {
+        markUp += `
+             <li id='${products[index]._id}' class="discount__item" data-product="true">
+            <svg class="discount__svg-discount">
+              <use href="#discount"></use>
+            </svg>
+            <div class="discount__box-img">
+              <img
+                src="${products[index].img}"
+                alt="${products[index].name}"
+                class="discount__image"
+              />
+            </div>
+            <div class="discount__svg-price">
+              <h3 class="discount__item-title">${products[index].name}</h3>
+              <div class="discount__item-wrap">
+                <p class="discount__price">$${products[index].price}</p>
+                <button data-productadd="true" class="discount__svg-container">
+                    ${
+                      JSON.parse(localStorage.getItem("cart"))
+                        .map((item) => item.id)
+                        .includes(products[index]._id)
+                        ? "✓"
+                        : `
+                        <svg class="discount__basket">
+                        <use href="#cart"></use>
+                        </svg>`
+                    }
+                    </button>
+              </div>
+            </div>
+          </li>
+          `
+        
+    }
+    document.querySelector(".discount__list").innerHTML = markUp
+})
---
+++ src/js/sections/index/order.js
@@ -0,0 +1,27 @@
+// const backdrop = document.querySelector(".order__modal");
+// const form = document.querySelector(".cart__form");
+// const closeButton = document.querySelector(".order__close-btn");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   backdrop.classList.toggle("is-hidden"); 
+//   document.body.style.overflow = "hidden";
+//   window.addEventListener("keydown", (event) => {
+//     if (event.key === "Escape") {
+//       backdrop.classList.add("is-hidden");
+//       document.body.style.overflow = "visible";
+//     }
+//   });
+// });
+
+// backdrop.addEventListener("click", (event) => {
+//   if (event.target === backdrop) {
+//     backdrop.classList.toggle("is-hidden");
+//     document.body.style.overflow = "visible";
+//   }
+// });
+
+// closeButton.addEventListener("click", () => {
+//   backdrop.classList.toggle("is-hidden");
+//   document.body.style.overflow = "visible";
+// });
