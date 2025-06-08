e05c8278eddbc0b21b3cca0368108775dde24279
Pizzaman333 <andrijmalchevv369@gmail.com>
2025-06-08 11:40:20 +0300

    Updates

--- cart.e087faca.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".basket__form").addEventListener("submit",e=>{e.preventDefault(),document.querySelector(".order__backdrop").classList.remove("order__hidden")}),document.querySelector(".order__close").addEventListener("click",e=>{e.preventDefault(),document.querySelector(".order__backdrop").classList.add("order__hidden")});
-//# sourceMappingURL=cart.e087faca.js.map
